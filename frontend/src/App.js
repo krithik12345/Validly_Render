39b2628b917e0800ea88a79fd28b27305a40ec58
krithik12345 <krishnankrithik7@gmail.com>
2025-08-07 22:58:26 -0500

    ok maybe this one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { supabase } from './supabaseClient';
 
 import HomePage from './components/HomePage';
